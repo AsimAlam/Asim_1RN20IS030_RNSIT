796c3521d85fa1706770f18ccda27b213aee417d
Asim <78923699+AsimAlam@users.noreply.github.com>
2024-03-01 11:20:20 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -8,7 +8,7 @@ if ($mysql_conn->connect_error) {
 
 
 // Mongo Instance
-$mongoManager = new MongoDB\Driver\Manager("mongodb://asimalam8:Asim123@ac-yogi2tv-shard-00-00.liakaku.mongodb.net:27017/fastfoodmern?ssl=true&replicaSet=atlas-lbmxxw-shard-0&authSource=admin&retryWrites=true&w=majority");
+$mongoManager = new MongoDB\Driver\Manager("mongodb://asimalam8:<PASSWORD>@ac-yogi2tv-shard-00-00.liakaku.mongodb.net:27017/fastfoodmern?ssl=true&replicaSet=atlas-lbmxxw-shard-0&authSource=admin&retryWrites=true&w=majority");
 $mongoDatabase = "guvi";
 $mongoCollection = "user";
 
